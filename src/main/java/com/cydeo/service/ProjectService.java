ebc8aa98642c6bd6a97b6ab788484e046cb14a7c
MutluBilgee <mutlubilgee@gmail.com>
2023-10-06 22:08:51 +0200

    Project create initial

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,8 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import org.springframework.stereotype.Controller;
+
+
+public interface ProjectService extends CrudService<ProjectDTO,String> {
+}
