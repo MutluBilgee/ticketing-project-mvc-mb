e5c54b5522e5eb9dfb801c18ce7e2d4b9d0bb0d4
MutluBilgee <mutlubilgee@gmail.com>
2023-10-07 07:28:59 +0200

    Update Delete Complete buttons

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,7 +27,7 @@ public class ProjectController {
 
        model.addAttribute("project",new ProjectDTO());
        model.addAttribute("projects",projectService.findAll());
-       model.addAttribute("managers",userService.findAll());
+       model.addAttribute("managers",userService.findManagers());
 
 
         return "/project/create";
@@ -39,4 +40,12 @@ public class ProjectController {
 
         return "redirect:/project/create";
     }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectcode){
+        projectService.deleteById(projectcode);
+        return "redirect:/project/create";
+    }
 }
+
+
